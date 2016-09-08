88a868144571343ad0a427b1685bd5104155a7c9
Peter Yoon <peyo.hd@gmail.com>
2016-09-08 21:00:48 +0900

    add MediaDrmPlayer  - copied from AOSP:/cts/tests/tests/media/src/android/media/cts/MediaCodecClearKeyPlayer.java

---
+++ src/com/peyo/drmplayer/MediaDrmActivity.java
@@ -0,0 +1,206 @@
+package com.peyo.drmplayer;
+
+import android.media.MediaDrm;
+import android.media.MediaDrmException;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.charset.Charset;
+import java.util.UUID;
+import java.util.Vector;
+
+public class MediaDrmActivity extends MediaCodecActivity {
+    private static final String TAG = "MediaDrmActivity";
+
+    private static final byte[] CLEAR_KEY_CENC =
+            { 0x1a, (byte)0x8a, 0x20, (byte)0x95, (byte)0xe4, (byte)0xde, (byte)0xb2, (byte)0xd2,
+                    (byte)0x9e, (byte)0xc8, 0x16, (byte)0xac, 0x7b, (byte)0xae, 0x20, (byte)0x82 };
+
+    public static final UUID CLEARKEY_SCHEME_UUID =
+            new UUID(0x1077efecc0b24d02L, 0xace33c1e52e2fb4bL);
+
+    private MediaDrm mDrm;
+    private byte[][] mClearKeys;
+    private byte[] mDrmInitData;
+    private byte[] mSessionId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mClearKeys = new byte[][]{ CLEAR_KEY_CENC };
+
+        try {
+            mDrm = new MediaDrm(CLEARKEY_SCHEME_UUID);
+            mDrm.setOnEventListener(new MediaDrm.OnEventListener() {
+                @Override
+                public void onEvent(MediaDrm md, byte[] sessionId, int event,
+                                    int extra, byte[] data) {
+                    if (event == MediaDrm.EVENT_KEY_REQUIRED) {
+                        Log.i(TAG, "MediaDrm event: Key required");
+                        getKeys(mDrm, "cenc", mSessionId, mDrmInitData, mClearKeys);
+                    } else if (event == MediaDrm.EVENT_KEY_EXPIRED) {
+                        Log.i(TAG, "MediaDrm event: Key expired");
+                        getKeys(mDrm, "cenc", mSessionId, mDrmInitData, mClearKeys);
+                    } else {
+                        Log.e(TAG, "Events not supported" + event);
+                    }
+                }
+            });
+        } catch (MediaDrmException e) {
+            Log.e(TAG, "Failed to create MediaDrm: " + e.getMessage());
+            finish();
+        }
+
+        if (!mDrm.isCryptoSchemeSupported(CLEARKEY_SCHEME_UUID)) {
+            mDrm.release();
+            Log.e(TAG, "Crypto scheme is not supported.");
+            finish();
+        }
+
+        boolean mRetryOpen;
+        do {
+            try {
+                mRetryOpen = false;
+                mSessionId = mDrm.openSession();
+            } catch (Exception e) {
+                mRetryOpen = true;
+            }
+        } while (mRetryOpen);
+    }
+
+    @Override
+    protected void playVideo() {
+        try {
+            mMediaCodecPlayer = new MediaDrmPlayer(mSessionId);
+            mMediaCodecPlayer.setDisplay(mSurfaceHolder);
+            setDataSource(mMediaCodecPlayer);
+            mMediaCodecPlayer.prepare();
+            mDrmInitData = ((MediaDrmPlayer)mMediaCodecPlayer).getDrmInitData();
+            getKeys(mDrm, "cenc", mSessionId, mDrmInitData, mClearKeys);
+
+            mMediaCodecPlayer.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void releaseMediaPlayer() {
+        if (mMediaCodecPlayer != null) {
+            mMediaCodecPlayer.release();
+            mMediaCodecPlayer = null;
+            mDrm.closeSession(mSessionId);
+            mDrm.release();
+        }
+    }
+
+    @Override
+    protected void setDataSource(MediaCodecPlayer player) throws Exception {
+        player.setDataSource("http://yt-dash-mse-test.commondatastorage.googleapis.com/media/car_cenc-20120827-88.mp4");
+    }
+
+    /**
+     * Retrieves clear key ids from getKeyRequest(), create JSON Web Key
+     * set and send it to the CDM via provideKeyResponse().
+     */
+    private void getKeys(MediaDrm drm, String initDataType,
+                         byte[] sessionId, byte[] drmInitData, byte[][] clearKeys) {
+        MediaDrm.KeyRequest drmRequest = null;;
+        try {
+            drmRequest = drm.getKeyRequest(sessionId, drmInitData, initDataType,
+                    MediaDrm.KEY_TYPE_STREAMING, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.i(TAG, "Failed to get key request: " + e.toString());
+        }
+        if (drmRequest == null) {
+            Log.e(TAG, "Failed getKeyRequest");
+            return;
+        }
+
+        Vector<String> keyIds = new Vector<String>();
+        if (0 == getKeyIds(drmRequest.getData(), keyIds)) {
+            Log.e(TAG, "No key ids found in initData");
+            return;
+        }
+
+        if (clearKeys.length != keyIds.size()) {
+            Log.e(TAG, "Mismatch number of key ids and keys: ids=" +
+                    keyIds.size() + ", keys=" + clearKeys.length);
+            return;
+        }
+
+        // Base64 encodes clearkeys. Keys are known to the application.
+        Vector<String> keys = new Vector<String>();
+        for (int i = 0; i < clearKeys.length; ++i) {
+            String clearKey = Base64.encodeToString(clearKeys[i],
+                    Base64.NO_PADDING | Base64.NO_WRAP);
+            keys.add(clearKey);
+        }
+
+        String jwkSet = createJsonWebKeySet(keyIds, keys);
+        byte[] jsonResponse = jwkSet.getBytes(Charset.forName("UTF-8"));
+
+        try {
+            try {
+                drm.provideKeyResponse(sessionId, jsonResponse);
+            } catch (IllegalStateException e) {
+                Log.e(TAG, "Failed to provide key response: " + e.toString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(TAG, "Failed to provide key response: " + e.toString());
+        }
+    }
+
+    /**
+     * Creates the JSON Web Key string.
+     *
+     * @return JSON Web Key string.
+     */
+    private String createJsonWebKeySet(Vector<String> keyIds, Vector<String> keys) {
+        String jwkSet = "{\"keys\":[";
+        for (int i = 0; i < keyIds.size(); ++i) {
+            String id = new String(keyIds.get(i).getBytes(Charset.forName("UTF-8")));
+            String key = new String(keys.get(i).getBytes(Charset.forName("UTF-8")));
+
+            jwkSet += "{\"kty\":\"oct\",\"kid\":\"" + id +
+                    "\",\"k\":\"" + key + "\"}";
+        }
+        jwkSet += "]}";
+        return jwkSet;
+    }
+
+    /**
+     * Extracts key ids from the pssh blob returned by getKeyRequest() and
+     * places it in keyIds.
+     * keyRequestBlob format (section 5.1.3.1):
+     * https://dvcs.w3.org/hg/html-media/raw-file/default/encrypted-media/encrypted-media.html#clear-key
+     *
+     * @return size of keyIds vector that contains the key ids, 0 for error
+     */
+    private int getKeyIds(byte[] keyRequestBlob, Vector<String> keyIds) {
+        if (0 == keyRequestBlob.length || keyIds == null)
+            return 0;
+
+        String jsonLicenseRequest = new String(keyRequestBlob);
+        keyIds.clear();
+
+        try {
+            JSONObject license = new JSONObject(jsonLicenseRequest);
+            final JSONArray ids = license.getJSONArray("kids");
+            for (int i = 0; i < ids.length(); ++i) {
+                keyIds.add(ids.getString(i));
+            }
+        } catch (JSONException e) {
+            Log.e(TAG, "Invalid JSON license = " + jsonLicenseRequest);
+            return 0;
+        }
+        return keyIds.size();
+    }
+}
---
+++ src/com/peyo/drmplayer/MediaDrmPlayer.java
@@ -0,0 +1,110 @@
+package com.peyo.drmplayer;
+
+import android.media.DrmInitData;
+import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaCodecList;
+import android.media.MediaCrypto;
+import android.media.MediaCryptoException;
+import android.media.MediaExtractor;
+import android.media.MediaFormat;
+import android.os.Build;
+
+import java.io.IOException;
+
+public class MediaDrmPlayer extends MediaCodecPlayer {
+    private static final String TAG = "MediaDrmPlayer";
+
+    private MediaCrypto mCrypto;
+
+    public MediaDrmPlayer(byte[] sessionId) throws MediaCryptoException {
+        mCrypto = new MediaCrypto(MediaDrmActivity.CLEARKEY_SCHEME_UUID, sessionId);
+    }
+
+    @Override
+    protected void addTrack(int trackIndex, MediaFormat format) throws IOException {
+        String mime = format.getString(MediaFormat.KEY_MIME);
+        MediaCodec codec;
+
+        if (mCrypto.requiresSecureDecoderComponent(mime)) {
+            codec = MediaCodec.createByCodecName(
+                    getSecureDecoderNameForMime(mime));
+        } else {
+            codec = MediaCodec.createDecoderByType(mime);
+        }
+
+        codec.configure(
+                format,
+                mSurfaceHolder.getSurface(),
+                mCrypto, 0);
+
+        CodecState state = new CodecState(this, mExtractor,
+                trackIndex, format, codec);
+        mCodecStates.put(Integer.valueOf(trackIndex), state);
+    }
+
+    public void release() {
+        super.release();
+        if (mCrypto != null) {
+            mCrypto.release();
+            mCrypto = null;
+        }
+    }
+
+    protected String getSecureDecoderNameForMime(String mime) {
+        MediaCodecInfo[] infos = new MediaCodecList(MediaCodecList.ALL_CODECS).getCodecInfos();
+        for(MediaCodecInfo info: infos) {
+            if (info.isEncoder()) {
+                continue;
+            }
+            String[] supportedTypes = info.getSupportedTypes();
+            for (int j = 0; j < supportedTypes.length; ++j) {
+                if (supportedTypes[j].equalsIgnoreCase(mime)) {
+                    return info.getName() + ".secure";
+                }
+            }
+        }
+        return null;
+    }
+
+    public final byte[] getDrmInitData() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            for (MediaExtractor ex : new MediaExtractor[]{mExtractor}) {
+                DrmInitData drmInitData = ex.getDrmInitData();
+                if (drmInitData != null) {
+                    DrmInitData.SchemeInitData schemeInitData = drmInitData.get(MediaDrmActivity.CLEARKEY_SCHEME_UUID);
+                    if (schemeInitData != null && schemeInitData.data != null) {
+                        return schemeInitData.data;
+                    }
+                }
+            }
+        }
+        // TODO
+        // Should not happen after we get content that has the clear key system id.
+        return PSSH;
+    }
+
+    private static final byte[] PSSH = hexStringToByteArray(
+            "0000003470737368" +  // BMFF box header (4 bytes size + 'pssh')
+                    "01000000" +          // Full box header (version = 1 flags = 0)
+                    "1077efecc0b24d02" +  // SystemID
+                    "ace33c1e52e2fb4b" +
+                    "00000001" +          // Number of key ids
+                    "60061e017e477e87" +  // Key id
+                    "7e57d00d1ed00d1e" +
+                    "00000000"            // Size of Data, must be zero
+    );
+
+    /**
+     * Convert a hex string into byte array.
+     */
+    private static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i + 1), 16));
+        }
+        return data;
+    }
+}
